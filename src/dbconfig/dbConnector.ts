9bd25c5b84bb64070a6a1ff43000a220ecaccca5
S-Lauren <sarah.ghanem.simplon@gmail.com>
2021-05-13 20:41:35 +0200

    add connection string instead

--- src/dbconfig/dbConnector.ts
+++ src/dbconfig/dbConnector.ts
@@ -1,13 +1,30 @@
 
-const Pool = require('pg').Pool;
+// const Pool = require('pg').Pool;
 require('dotenv').config()
-const pool = new Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DB,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT
-});
-pool.connect()
+// const pool = new Pool({
+//     user: process.env.PG_USER,
+//     host: process.env.PG_HOST,
+//     database: process.env.PG_DB,
+//     password: process.env.PG_PASSWORD,
+//     port: process.env.PG_PORT
+// });
+// pool.connect()
 
+// module.exports = pool;
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+
+});
+
+pool.connect();
+
+pool.query('SELECT * FROM pizza', (err: any, res: any) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+
+});
 module.exports = pool;
