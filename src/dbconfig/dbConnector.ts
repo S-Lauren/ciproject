a501f9e7dde3669dc4a391f8c6631fa2070ebef7
S-Lauren <sarah.ghanem.simplon@gmail.com>
2021-05-16 16:06:07 +0200

    change to production

--- src/dbconfig/dbConnector.ts
+++ src/dbconfig/dbConnector.ts
@@ -4,7 +4,7 @@ require('dotenv').config()
 // const pool = new Pool({
 //     user: process.env.PG_USER,
 //     host: process.env.PG_HOST,
-//     database: process.env.PG_DB,
+//     database: process.env.PG_PG,
 //     password: process.env.PG_PASSWORD,
 //     port: process.env.PG_PORT
 // });
@@ -13,11 +13,23 @@ require('dotenv').config()
 // module.exports = pool;
 const { Pool } = require('pg');
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
 
-});
+const isProduction = process.env.NODE_ENV === 'production'
+
+const connectionString =
+    `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}
+@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DB}`
+
+const pool = new Pool({
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: true,
+})
+
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: true,
+
+// });
 
 pool.connect();
 
