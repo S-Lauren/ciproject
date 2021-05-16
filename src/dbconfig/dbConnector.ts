224a5da856d967da5a76ec44adbfc6f3f493f235
S-Lauren <sarah.ghanem.simplon@gmail.com>
2021-05-16 10:26:28 +0200

    change config db

--- src/dbconfig/dbConnector.ts
+++ src/dbconfig/dbConnector.ts
@@ -16,9 +16,6 @@ const { Pool } = require('pg');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: true,
-    strictSSL: false,
-    rejectUnauthorized: false
-
 });
 
 pool.connect();
