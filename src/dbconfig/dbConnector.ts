f1b81cb83c2a622266edee15a24b0fdc29c384e1
S-Lauren <sarah.ghanem.simplon@gmail.com>
2021-05-21 21:02:07 +0200

    test

--- src/dbconfig/dbConnector.ts
+++ src/dbconfig/dbConnector.ts
@@ -1,41 +1,15 @@
 
-// const Pool = require('pg').Pool;
-require('dotenv').config()
-// const pool = new Pool({
-//     user: process.env.PG_USER,
-//     host: process.env.PG_HOST,
-//     database: process.env.PG_PG,
-//     password: process.env.PG_PASSWORD,
-//     port: process.env.PG_PORT
-// });
-// pool.connect()
-
-// module.exports = pool;
 const { Pool } = require('pg');
-
+require('dotenv').config()
 
 const isProduction = process.env.NODE_ENV === 'production'
 
-const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DB}`
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 const pool = new Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
     ssl: isProduction,
 })
 
-// const pool = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: true,
 
-// });
-
-pool.connect();
-
-pool.query('SELECT * FROM pizza', (err: any, res: any) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-        console.log(JSON.stringify(row));
-    }
-
-});
-module.exports = pool;
+module.exports = { pool }
