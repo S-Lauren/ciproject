a224d9e2e3c7f4be89cd52ae7eef8da010323567
S-Lauren <sarah.ghanem.simplon@gmail.com>
2021-05-23 10:31:43 +0200

    test if now production mode works

--- src/server.ts
+++ src/server.ts
@@ -5,35 +5,49 @@ import { Request, Response } from 'express'
 const app = express()
 const server = require('http').createServer(app)
 const cors = require("cors");
-const pizzaRoutes = require("./routes/pizzaRoutes");
-
+// const pizzaRoutes = require("./routes/pizzaRoutes");
+const { pool } = require("./dbconfig/dbConnector")
 
 const PORT = process.env.PORT || 5000
 
 // app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded());
-app.use("/", pizzaRoutes);
+// app.get("/pizzas", pizzaRoutes);
 
 
-app.get("/", async (req, res: Response) => {
-    try {
-        res.send({ ok: true })
-    } catch (e) {
-        console.log(e)
-        res.status(500).json()
-    }
-})
+// app.get("/", async (req, res: Response) => {
+//   try {
+//     res.send({ ok: true })
+//   } catch (e) {
+//     console.log(e)
+//     res.status(500).json()
+//   }
+// })
 
 // app.get('/index', (request, response) => {
 //     response.sendfile('./src/index.html');
 // });
 
+const getPizza = (request: Request, response: Response) => {
+  pool.query('SELECT * FROM pizza', (error: any, results: any) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+app
+  .route('/pizzas')
+  // GET endpoint
+  .get(getPizza)
 
 
+// Origin for cors
 const isProduction = process.env.NODE_ENV === 'production'
 const origin = {
-    origin: isProduction ? 'https://ci-app-node.herokuapp.com/' : '*'
+  origin: isProduction ? 'https://ci-app-node.herokuapp.com' : '*'
 }
 
 app.use(cors(origin))
