3954e5c82a9f399f0f6365e38b1b256560cd076e
S-Lauren <sarah.ghanem.simplon@gmail.com>
2021-05-24 14:29:48 +0200

    test endpoint GET/API route

--- src/routes/__tests__/pizzaRoutes.test.ts
+++ src/routes/__tests__/pizzaRoutes.test.ts
@@ -9,23 +9,16 @@ it('Testing to see if Jest works', () => {
   expect(1).toBe(1)
 })
 
-// describe('pizzaRoutes', () => {
-//   it("should respond with status 201 if no error ", async () => {
-//     // const pizza = await pizzaController.addPizza({ title: "Reine", toppings: "mozza" });
-
-
-
-//     const res = await supertest(app)
-//       .get('/pizzas')
-//     expect(res.statusCode).toEqual(201)
-//     expect(res.body.length).toBe(1);
-//     expect(res.body[0]).toHaveProperty("id");
-//     expect(res.body[0]).toHaveProperty("name");
-//     expect(res.body[0]).toHaveProperty('toppings')
-//     expect(res.body[0]).toHaveProperty('price')
-
-
-
-//   })
-// })
+describe('pizzaRoutes', () => {
+  it("should display a pizza 🍕 ", async () => {
+    const res = await supertest(app)
+      .get('/pizzas')
+    expect(res.statusCode).toEqual(201)
+    expect(res.body.length).toBe(1);
+    expect(res.body[0]).toHaveProperty("id");
+    expect(res.body[0]).toHaveProperty("name");
+    expect(res.body[0]).toHaveProperty('toppings')
+    expect(res.body[0]).toHaveProperty('price')
+  })
+})
 
